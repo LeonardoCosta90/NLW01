52d7e655c8ab388f25a5a9036e0e280c6e007c1b
unknown <leonardoeverton.tec@gmail.com>
2020-12-21 09:59:38 -0300

    Banco de dados e conceitos avançados

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,91 +1,77 @@
-// Endpoint https://servicodados.ibge.gov.br/api/v1/localidades/estados
-
 function populateUFs() {
-  const ufSelect = document.querySelector("select[name=uf]")
-  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-  .then( res => res.json() )
-  .then( states => {
+  const ufSelect = document.querySelector('select[name=uf]');
 
-    for( let state of states ) {
-      ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-    } 
-  })
+  fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    .then(res => {
+      return res.json();
+    })
+    .then(states => {
+      for (const state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+      }
+    });
 }
 
-populateUFs()
+populateUFs();
 
-function getCities(event) {
-  const citySelect = document.querySelector("select[name=city]")
-  const stateInput = document.querySelector("input[name=state]")
+function getCities() {
+  const citySelect = document.querySelector('[name=city]');
+  const stateInput = document.querySelector('[name=state]');
 
-  const ufValue = event.target.value
+  const ufValue = event.target.value;
 
+  const indexOfSelectedState = event.target.selectedIndex;
+  stateInput.value = event.target.options[indexOfSelectedState].text;
 
-  const indexOfSelectedState = event.target.selectedIndex
-  stateInput.value = event.target.options[indexOfSelectedState].text
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
-  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  citySelect.innerHTML = '<option value>Selecione sua cidade</option>';
+  citySelect.disabled = true;
 
-  citySelect.innerHTML = "<option>Selecione a cidade</option>"
-  citySelect.disabled = true
-  
   fetch(url)
-  .then( res => res.json() )
-  .then( cities => {
-    
-    for( const city of cities ) {
-      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
-    } 
-
-    citySelect.disabled = false
-
-  })
+    .then(res => {
+      return res.json();
+    })
+    .then(cities => {
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
+      }
+      citySelect.disabled = false;
+    });
 }
 
+document.querySelector('select[name=uf]').addEventListener('change', getCities);
 
-document
-  .querySelector("select[name=uf]")
-  .addEventListener("change", getCities)
+const itemsToCollect = document.querySelectorAll('.items-grid li');
 
+for (const item of itemsToCollect) {
+  item.addEventListener('click', handleSelectedItem);
+}
 
-  const itemsToCollect = document.querySelectorAll(".items-grid li")
+const collectedItems = document.querySelector('input[name=items]');
 
-  for(let item of itemsToCollect) {
-    item.addEventListener("click", handleSelectedItem)
+let selectedItems = [];
 
-  }
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+  const itemID = itemLi.dataset.id;
 
-  const collectedItems = document.querySelector("input[name=item]")
-  
-  let selectedItems = []
+  itemLi.classList.toggle('selected');
 
+  const alreadySelected = selectedItems.findIndex(item => {
+    const itemFound = item === itemID;
+    return itemFound;
+  });
 
-  function handleSelectedItem (event) {
-    const itemLi = event.target
-    
-    itemLi.classList.toggle("selected")
-
-    const itemId = itemLi.dataset.id
-    
-    const alreadySelected = selectedItems.findIndex( item => {
-      const itemFound = item == itemId
-      return itemFound
-    })
-  
-
-
-  if(alreadySelected >= 0) {
-    let filteredItems = selectedItems.filter( item => {
-      let itemIsDifferent = item != itemId 
-      return itemIsDifferent
-    })
-
-    selectedItems = filteredItems
-
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter(item => {
+      const itemIsDifferent = item !== itemID;
+      return itemIsDifferent;
+    });
+    selectedItems = filteredItems;
   } else {
-    selectedItems.push(itemId)
+    selectedItems.push(itemID);
   }
 
-  collectedItems.value = selectedItems
-
-  }
+  collectedItems.value = selectedItems;
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const server = express();
+const database = require('./database/database');
 server.use(express.static('public'));
 server.use(express.urlencoded({ extended: true }));
 
@@ -16,7 +17,63 @@ server.get('/create-point', (req, res) => {
   return res.render('create-point.html');
 });
 server.get('/search-results', (req, res) => {
+  const search = req.query.search;
+
+  if (search === '') {
     return res.render('search-results.html', { total: 0 });
+  }
+
+  database.all(
+    `SELECT * FROM places  WHERE city LIKE '${search}%'  `,
+    function (err, rows) {
+      if (err) {
+        console.log(err);
+      }
+
+      const total = rows.length;
+
+      console.log('Aqui estão seus registros: ');
+      console.log(rows);
+
+      return res.render('search-results.html', { places: rows, total });
+    },
+  );
 });
 
+server.post('/save-point', (req, res) => {
+  console.log(req.body);
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?)
+  `;
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items,
+  ];
+
+  function afterInsertData(error) {
+    if (error) {
+      console.log(error);
+      return res.render('create-point.html', { error: true });
+    }
+    console.log('Cadastrado com sucesso!');
+    console.log(this);
+
+    return res.render('create-point.html', { saved: true });
+  }
+
+  database.run(query, values, afterInsertData);
+});
 server.listen(3000);
